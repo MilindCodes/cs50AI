dba5e41792c59bd2ad1d343df298273c63abbf9d
Milind Chaturvedi <milindchaturvedi@MacBook-Pro-2.lan>
2024-08-25 09:23:07 -0400

    adding some code from coding course

--- maze.py
+++ maze.py
@@ -1,2 +1,34 @@
-print("hello world")
-print("hello, back to ya")
+'''
+None of this is original code - this is all from the course I'm doing
+'''
+
+import sys
+
+
+class Node:
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier: #LIFO data structure
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+    
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+    
+    def empty(self):
+        return len(self.frontier) == 0
+    
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier"):
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
